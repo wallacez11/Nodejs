dcce23aa332810bd7aa718866534ec7b6096e952
wallacez11 <wallace.santos.mail@gmail.com>
2023-11-22 19:33:27 -0300

    implementing routes

--- 07tut/server.js
+++ 07tut/server.js
@@ -36,47 +36,15 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname , '/public')))
+app.use('/subdir',express.static(path.join(__dirname , '/public')))
 
-app.get('^/$|index(.html)?', (req, res) =>{
-    //res.sendFile('./views/index.html', {root: __dirname})
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+//routes
 
-});
-app.get('/new-page(.html)?', (req, res) =>{
-    
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
-
-});
-app.get('/old-page(.html)?', (req, res) =>{
-    
-    res.redirect(301, './new-page.html')
-
-});
-
-app.get('/hello(.html)?', (req, res, next) =>{
-    console.log('attempted to load hello.html')
-    next()
-}, (req, res) =>{
-    res.send('hello wor')
-})
+app.use('/',require('./routes/roots'))
+app.use('/subdir',require('./routes/subdir'))
+app.use('/employees',require('./routes/api/employees'))
 
 
-const one = (req, res, next) =>{
-    console.log('one')
-    next()
-}
-
-const two = (req, res, next) =>{
-    console.log('two')
-    next()
-}
-
-const three = (req, res) =>{
-    console.log('three')
-    res.send('Finished!')
-}
-
-app.get('/chain(.html)?',[one, two, three])
 
 
 app.all('*', (req, res) =>{
