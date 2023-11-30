a262857ddee26b609332503827e1a6c5ca2ebdfb
wallacez11 <wallace.santos.mail@gmail.com>
2023-11-29 21:55:05 -0300

    removing unnecessary console.log

--- WebServer/middleware/errorHandler.js
+++ WebServer/middleware/errorHandler.js
@@ -2,7 +2,7 @@ const {logEvents} = require('./logEvents')
 
 const errorHandler = (err, req, res, next) => {
     logEvents(` ${err.message}`, 'errLog.txt')
-    console.error(err.stack)
+
     res.status(500).send(err.message)
 }
 
