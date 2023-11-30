5403649dcf53056615b2f0a4c3dc285f83dd3aba
wallacez11 <wallace.santos.mail@gmail.com>
2023-11-30 18:45:38 -0300

    creating user

--- WebServer/controllers/registerController.js
+++ WebServer/controllers/registerController.js
@@ -1,10 +1,4 @@
-const userDb = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data}
-}
-
-const fsPromises = require('fs').promises
-const path = require('path')
+const User = require('./../model/User')
 const bcrypt = require('bcrypt')
 const roles = require('../config/roles_list')
 
@@ -12,21 +6,15 @@ const handlerNewUser = async(req, res) =>{
     const{user, pwd} = req.body
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required'})
     // check for duplicate username in the db
-    const duplicate = userDb.users.find(person => person.username === user)
+    const duplicate = await User.findOne({username: user}).exec()
+
     if (duplicate) return res.sendStatus(409)
 
     try{
         const hashedPwd = await bcrypt.hash(pwd, 10)
 
-        const newUser = {"username": user, "password": hashedPwd, "roles":[roles.User]}
-
-        userDb.setUsers([...userDb.users, newUser])
-
-        await fsPromises.writeFile(
-            path.join(__dirname, '..','model', 'users.json'),
-            JSON.stringify(userDb.users)
-        )
-        console.log(userDb.users)
+        const result = await User.create({"username": user, "password": hashedPwd})
+        console.log(result)
         res.status(201).json({'sucess': `New user ${user} created!`})
     }catch(err){
         res.status(500).json({'message': err.message})
---
+++ WebServer/model/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: { type: String, required: true },
+    roles: { type: [String], default: ["user"], required: true },
+    password: { type: String, required: true },
+    refreshToken: { type: String }
+})
+
+module.exports = mongoose.model('User', userSchema)
